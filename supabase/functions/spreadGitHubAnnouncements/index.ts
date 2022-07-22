1ff34b0e52ac9640aa17a37f180cb1fb9f27ff2a
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-07-22 21:47:24 +0200

    Added webhook for posting GitHub announcements

---
+++ supabase/functions/spreadGitHubAnnouncements/index.ts
@@ -0,0 +1,26 @@
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
+import { corsAll as headers } from '../_shared/responseHeaders.ts'
+
+serve(async (req) => {
+	if(req.method === 'OPTIONS'){
+		return new Response('ok', { headers: headers })
+	}
+
+	const { action, discussion } = await req.json()
+
+	let isCreated = action === 'created'
+	let isAnnouncement = discussion.category.id === 37846981
+
+	if(isCreated && isAnnouncement){
+		fetch(new Request('https://discord.com/api/webhooks/1000065546252992602/F9dp4eUOMwe7MqRJCr6dyT8Y8Luzh72lWt9rnfwf5jR_8QqX3jNzPm1wPSm9rRCtxkJh', {
+			method: 'POST',
+			headers: new Headers({
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}),
+			body: JSON.stringify({
+				content: '**'+discussion.title+'**\n'+discussion.body
+			})
+		}))
+	}
+})
